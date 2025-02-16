bcc5698659c51396964e8477ccac351acb9caf4e
Yatri Trivedi <jivanyatra@gmail.com>
2025-02-16 02:08:28 -0500

    added job registries (which aren't iterable!); swapped do_something in as a test function for queuing; added new routes to view jobs

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from bottle import get, post, request, route, run, static_file, view
 from loguru import logger
 from os import path
 from rq import Queue
+from rq.registry import StartedJobRegistry, FailedJobRegistry, FinishedJobRegistry
 from time import sleep
 from tools import do_something, just_return, test_generator
 
@@ -10,6 +11,9 @@ import redis
 ROOT = path.abspath(path.dirname(__file__))
 r = redis.Redis(host="redis", port=6379)
 q = Queue(connection=r)
+started_registry = StartedJobRegistry(queue=q)
+failed_registry = FailedJobRegistry(queue=q)
+finished_registry = FinishedJobRegistry(queue=q)
 
 
 @route("/static/<filepath>")
@@ -54,12 +58,22 @@ def process(
     #     yield f'<div id="results" hx-trigger="every 1s" hx-target="this" hx-swap="outerHTML"><p>{line}</p></div>'
     for _ in request.forms["number_of_times"]:
         task = q.enqueue(
-            test_generator, text=f"{request.forms["input_field"]}", number=10
+            # test_generator, text=f"{request.forms["input_field"]}", number=10
+            do_something,
+            text=f"{request.forms["input_field"]}",
+            number=10,
         )
 
     for job in q.jobs:
         link = f'<a href="/hello/{name}/job/{job.id}">{job._status}</a>'
         yield f'<div id="results" hx-trigger="every 1s" hx-target="this" hx-swap="outerHTML"><p>{job.id} - {link}</p></div>'
+
+
+@route("/<name>/jobs")
+def jobs(name="Default Name"):
+    for job in q.jobs:
+        link = f'<a href="/hello/{name}/job/{job.id}">{job._status}</a>'
+        yield f'<div id="jobs" hx-trigger="every 1s" hx-target="this" hx-swap="outerHTML"><p>{job.id} - {link}</p></div>'
 
 
 @route("/hello/<name>/job/<job_id>")
@@ -69,6 +83,21 @@ def job(name, job_id):
         return f"<center><br /><br /><h3>The job is still pending</h3><br /><br />ID:{job_id}<br />Queued at: {job.enqueued_at}<br />Status: {job._status}</center>"
     else:
         return f'<center><br /><br /><img src="{job.result}" height="200px"><br /><br />ID:{job_id}<br />Queued at: {job.enqueued_at}<br />Finished at: {job.ended_at}</center>'
+
+
+@route("/<name>/done")
+def done(name="Default Name"):
+    # failed_jobs = failed_registry.get_job_ids()
+    # finished_jobs = finished_registry.get_job_ids()
+
+    for job in failed_registry:
+        result = job.latest_result()
+        link = f'<a href="/hello/{name}/job/{job.id}">{result.exc_string}</a>'
+        yield f'<div id="jobs" hx-trigger="every 1s" hx-target="this" hx-swap="outerHTML"><p>{job.id} - {link}</p></div>'
+    for job in finished_registry:
+        result = job.latest_result()
+        link = f'<a href="/hello/{name}/job/{job.id}">{result.return_value}</a>'
+        yield f'<div id="jobs" hx-trigger="every 1s" hx-target="this" hx-swap="outerHTML"><p>{job.id} - {link}</p></div>'
 
 
 @route("/hello/<name>/running")
